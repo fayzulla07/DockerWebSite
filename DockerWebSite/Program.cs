2b26473e2f88a1457383e182b94539dc4660936d
ctrl_13 <ctrl_13@list.ru>
2023-12-25 17:54:52 +0600

    add-health check

--- DockerWebSite/Program.cs
+++ DockerWebSite/Program.cs
@@ -1,15 +1,31 @@
 
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 public class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
+        builder.Services.AddHealthChecks()
+            .AddSqlServer(builder.Configuration.GetValue<string>("ConnectionStrings:DefaultConnection"));
+        builder.Services.AddHealthChecksUI().AddInMemoryStorage();
         // Add services to the container.
         builder.Services.AddRazorPages();
 
         var app = builder.Build();
+        app.UseHealthChecks("/healthcheck", new HealthCheckOptions
+        {
+            Predicate = _ => true,
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse //nuget: AspNetCore.HealthChecks.UI.Client
+        });
 
+        //nuget: AspNetCore.HealthChecks.UI
+        app.UseHealthChecksUI(options =>
+        {
+            options.UIPath = "/hc-ui";
+            options.ApiPath = "/health-ui-api";
+        });
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
